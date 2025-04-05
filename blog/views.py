9ca1f2d1f552eb24a83991d23ac25f083a6a44fd
daniilstudenikin <d.studenikin@action.tech>
2025-04-05 12:06:19 +0300

    Add base structure with header, footer, and initial templates for blog

--- blog/views.py
+++ blog/views.py
@@ -44,10 +44,10 @@ posts = [
 ]
 
 def index(request):
-    return render(request, 'pages/index.html')
+    return render(request, 'blog/index.html')
 
 def post_detail(request, post_id):
-    return render(request, 'pages/post_detail.html')
+    return render(request, 'blog/detail.html')
 
 def category_posts(request, category_slug):
-    return render(request, 'pages/category_posts.html')
+    return render(request, 'blog/category.html')
